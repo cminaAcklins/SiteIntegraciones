2ecbd6cdc1ba35b7b7e210ae6d240e1f74364094
cminaAcklins <christian.mina@acklins.net>
2025-10-01 14:01:59 -0500

    Ajustes

--- obj/Debug/net8.0/Integraciones.AssemblyInfo.cs
+++ obj/Debug/net8.0/Integraciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Integraciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21cd4348e8317eb6a1457d42443d5c25acca0c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Integraciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Integraciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
